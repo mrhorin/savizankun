df9ae83f8b50a82d45b41b2dd2fc1bcfd45a5da6
mrhorin <u0813mb@gmail.com>
2016-01-05 12:09:10 +0900

    index.htmlのベース部分を実装

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,19 @@
 (function() {
-  var hoge;
+  var shareFacebook, shareHatena, shareTwitter;
 
-  hoge = "savizankun";
+  shareTwitter = function() {
+    window.open(this.href, 'Twitter', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes');
+    return false;
+  };
+
+  shareFacebook = function() {
+    window.open(this.href, 'Faceboo', 'width=550, height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+    return false;
+  };
+
+  shareHatena = function() {
+    window.open(this.href, 'Hatena', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes');
+    return false;
+  };
 
 }).call(this);
