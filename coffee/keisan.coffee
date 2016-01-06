1252573c2e4ec61a67c82843fc6bbdaad9b22167
mrhorin <u0813mb@gmail.com>
2016-01-06 16:55:05 +0900

    各種労働時間を計算できるように

--- coffee/keisan.coffee
+++ coffee/keisan.coffee
@@ -6,6 +6,8 @@ class window.Keisan
   WARIMASHI = 0.25
   # 法定労働時間
   HOUTEI_ROUDOU_ZIKAN = 8
+  # 一週間の法定労働時間
+  WEEK_HOUTEI_ROUDOU_ZIKAN = 40
   # 割増賃金が発生する深夜時間帯
   START_SHINYA = 22
   END_SHINYA = 5
@@ -20,21 +22,67 @@ class window.Keisan
 
     @rodoTime =
       # 所定労働時間
-      "syotei": _getRodoTime(@formValues["startWork"], @formValues["endWork"], @formValues["kyukei"])
+      "syotei": _getBetweenTime(@formValues["startWork"], @formValues["endWork"], @formValues["kyukei"])
       # 実労働時間
-      "zitu": _getRodoTime(@formValues["startWork"], @formValues["overEndWork"], @formValues["kyukei"])
+      "zitu": _getBetweenTime(@formValues["startWork"], @formValues["overEndWork"], @formValues["kyukei"])
+    # 所定労働時間の超過時間
+    @rodoTime["overSyotei"] = @rodoTime["zitu"] - @rodoTime["syotei"]
+    # 法定労働時間の超過時間
+    @rodoTime["overHoutei"] = _getOverHouteiRodoTime(@rodoTime["zitu"])
+    # 法定労働時間の超過時間（週単位）
+    @rodoTime["overWeekHoutei"] = _getWeekOverHouteiRodoTime(@rodoTime["zitu"], @formValues["syukyu"])
+    # 深夜労働時間
+    @rodoTime["shinya"] = _getShinyaRodoTime(@formValues["startWork"], @formValues["kyukei"], @rodoTime["zitu"], @rodoTime["syotei"])
+    # console.log @rodoTime
 
-    console.log @formValues
-    console.log @rodoTime
+  # 時給を取得
+  getZikyu: ->
+    _zikyu = @formValues["gekkyu"] / ((365 - @formValues["nenkyu"]) * @rodoTime["syotei"] / 12)
+    return Math.round _zikyu
+
+  # 深夜労働時間を取得
+  _getShinyaRodoTime = (startWork, kyukei, zituRodoTime, syoteiRodoTime)->
+    _kyukei = kyukei / 60 # 休憩時間を時間単位に変換
+    _shinyaRodoTime = 0
+    _startShinyaCount = startWork + _kyukei + syoteiRodoTime
+    _endShinyaCount = startWork + _kyukei + zituRodoTime
+
+    _i = _startShinyaCount + 0.25 # 初期値
+    while _i <= _endShinyaCount
+      if( (0<_i && _i<=5) || (22<_i && _i<=29) || (46<_i && _i<=48) )
+        _shinyaRodoTime += 0.25
+      _i += 0.25 # 増分
+
+    return _shinyaRodoTime
 
   # 時刻を小数値に変換して取得
   _timeParseFloat = (hour, min)->
     return min / 60 + hour
 
   # startTimeからendTimeまでの労働時間を取得
-  _getRodoTime = (startTime, endTime, kyukei)->
-    kyukei = kyukei / 60
+  _getBetweenTime = (startTime, endTime, kyukei)->
+    # 休憩時間を時間単位に変換
+    _kyukei = kyukei / 60
     if(endTime - startTime > 0)
-      return endTime - startTime - kyukei
+      return endTime - startTime - _kyukei
     else
-      return 24 - startTime + endTime - kyukei
+      return 24 - startTime + endTime - _kyukei
+
+  # 法定労働時間の超過時間を取得
+  _getOverHouteiRodoTime = (zituRodoTime)->
+    if zituRodoTime >= HOUTEI_ROUDOU_ZIKAN
+      return zituRodoTime - HOUTEI_ROUDOU_ZIKAN
+    else
+      return 0
+
+  # 法定労働時間の超過時間を取得（週単位）
+  _getWeekOverHouteiRodoTime = (zituRodoTime, syukyu)->
+    # 一週間の労働日数
+    _rodoDays = 7 - syukyu
+    # 一週間の実労働時間
+    _weekZituRodoTime = zituRodoTime * _rodoDays
+
+    if _weekZituRodoTime > WEEK_HOUTEI_ROUDOU_ZIKAN
+      return _weekZituRodoTime - WEEK_HOUTEI_ROUDOU_ZIKAN
+    else
+      return 0
--- public/scripts.js
+++ public/scripts.js
@@ -100,13 +100,15 @@
 
 (function() {
   window.Keisan = (function() {
-    var END_SHINYA, HOUTEI_ROUDOU_ZIKAN, START_SHINYA, WARIMASHI, WEEKS, _getRodoTime, _timeParseFloat;
+    var END_SHINYA, HOUTEI_ROUDOU_ZIKAN, START_SHINYA, WARIMASHI, WEEKS, WEEK_HOUTEI_ROUDOU_ZIKAN, _getBetweenTime, _getOverHouteiRodoTime, _getShinyaRodoTime, _getWeekOverHouteiRodoTime, _timeParseFloat;
 
     WEEKS = 4.4;
 
     WARIMASHI = 0.25;
 
     HOUTEI_ROUDOU_ZIKAN = 8;
+
+    WEEK_HOUTEI_ROUDOU_ZIKAN = 40;
 
     START_SHINYA = 22;
 
@@ -118,23 +120,67 @@
       this.formValues["endWork"] = _timeParseFloat(this.formValues["endWorkHour"], this.formValues["endWorkMin"]);
       this.formValues["overEndWork"] = _timeParseFloat(this.formValues["overEndWorkHour"], this.formValues["overEndWorkMin"]);
       this.rodoTime = {
-        "syotei": _getRodoTime(this.formValues["startWork"], this.formValues["endWork"], this.formValues["kyukei"]),
-        "zitu": _getRodoTime(this.formValues["startWork"], this.formValues["overEndWork"], this.formValues["kyukei"])
+        "syotei": _getBetweenTime(this.formValues["startWork"], this.formValues["endWork"], this.formValues["kyukei"]),
+        "zitu": _getBetweenTime(this.formValues["startWork"], this.formValues["overEndWork"], this.formValues["kyukei"])
       };
-      console.log(this.formValues);
-      console.log(this.rodoTime);
+      this.rodoTime["overSyotei"] = this.rodoTime["zitu"] - this.rodoTime["syotei"];
+      this.rodoTime["overHoutei"] = _getOverHouteiRodoTime(this.rodoTime["zitu"]);
+      this.rodoTime["overWeekHoutei"] = _getWeekOverHouteiRodoTime(this.rodoTime["zitu"], this.formValues["syukyu"]);
+      this.rodoTime["shinya"] = _getShinyaRodoTime(this.formValues["startWork"], this.formValues["kyukei"], this.rodoTime["zitu"], this.rodoTime["syotei"]);
     }
+
+    Keisan.prototype.getZikyu = function() {
+      var _zikyu;
+      _zikyu = this.formValues["gekkyu"] / ((365 - this.formValues["nenkyu"]) * this.rodoTime["syotei"] / 12);
+      return Math.round(_zikyu);
+    };
+
+    _getShinyaRodoTime = function(startWork, kyukei, zituRodoTime, syoteiRodoTime) {
+      var _endShinyaCount, _i, _kyukei, _shinyaRodoTime, _startShinyaCount;
+      _kyukei = kyukei / 60;
+      _shinyaRodoTime = 0;
+      _startShinyaCount = startWork + _kyukei + syoteiRodoTime;
+      _endShinyaCount = startWork + _kyukei + zituRodoTime;
+      _i = _startShinyaCount + 0.25;
+      while (_i <= _endShinyaCount) {
+        if ((0 < _i && _i <= 5) || (22 < _i && _i <= 29) || (46 < _i && _i <= 48)) {
+          _shinyaRodoTime += 0.25;
+        }
+        _i += 0.25;
+      }
+      return _shinyaRodoTime;
+    };
 
     _timeParseFloat = function(hour, min) {
       return min / 60 + hour;
     };
 
-    _getRodoTime = function(startTime, endTime, kyukei) {
-      kyukei = kyukei / 60;
+    _getBetweenTime = function(startTime, endTime, kyukei) {
+      var _kyukei;
+      _kyukei = kyukei / 60;
       if (endTime - startTime > 0) {
-        return endTime - startTime - kyukei;
+        return endTime - startTime - _kyukei;
       } else {
-        return 24 - startTime + endTime - kyukei;
+        return 24 - startTime + endTime - _kyukei;
+      }
+    };
+
+    _getOverHouteiRodoTime = function(zituRodoTime) {
+      if (zituRodoTime >= HOUTEI_ROUDOU_ZIKAN) {
+        return zituRodoTime - HOUTEI_ROUDOU_ZIKAN;
+      } else {
+        return 0;
+      }
+    };
+
+    _getWeekOverHouteiRodoTime = function(zituRodoTime, syukyu) {
+      var _rodoDays, _weekZituRodoTime;
+      _rodoDays = 7 - syukyu;
+      _weekZituRodoTime = zituRodoTime * _rodoDays;
+      if (_weekZituRodoTime > WEEK_HOUTEI_ROUDOU_ZIKAN) {
+        return _weekZituRodoTime - WEEK_HOUTEI_ROUDOU_ZIKAN;
+      } else {
+        return 0;
       }
     };
 
